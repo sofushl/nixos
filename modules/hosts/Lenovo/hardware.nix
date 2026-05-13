9f7f0110ff6047574fb5059592d22dd6b8451382
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-05-13 21:26:30 +0200

    remove ipv6 from Lenovo pc

--- modules/hosts/Lenovo/hardware.nix
+++ modules/hosts/Lenovo/hardware.nix
@@ -28,6 +28,14 @@
       boot.extraModulePackages = [ ];
       boot.kernelParams = [ "intel_pstate=active" ];
 
+      networking.enableIPv6 = false;
+
+      boot.kernel.sysctl = {
+        "net.ipv6.conf.all.disable_ipv6" = 1;
+        "net.ipv6.conf.default.disable_ipv6" = 1;
+        "net.ipv6.conf.lo.disable_ipv6" = 1;
+      };
+
       fileSystems."/" = {
         device = "/dev/disk/by-uuid/96002411-4977-4179-afd6-1bf45f4a5a8c";
         fsType = "xfs";
