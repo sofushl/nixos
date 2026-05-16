a6d095e300d4e62b041e563d2c58abb9d62bc4c6
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-05-16 13:21:13 +0200

    updated imports (might break wsl)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
-  description = "A very basic flake";
+  description = "Multi system dendritic NixOS config";
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -18,21 +20,36 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    #stylix = {
-    #  url = "github:nix-community/stylix";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    #impermanence = {
-    #  url = "github:nix-community/impermanence";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
+    preservation = {
+      url = "github:nix-community/preservation";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = true;
+    };
+
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
 
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "flake-compat";
     };
-
   };
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
