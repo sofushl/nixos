f88ac5d6220ae18032cd9b4ccdc1432ebfc5df1d
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-06-06 19:54:41 +0200

    init config for low ram installations with disko

---
+++ modules/hosts/presets/init.nix
@@ -0,0 +1,41 @@
+{ self, inputs, ... }:
+let
+  userconf = import ../../../lib/sofushl.nix;
+  sysconf = import ../../../lib/PLACEHOLDER.nix;
+  sshkeys = import ../../../lib/sshkeys.nix;
+  secrets = import /etc/nixos/secrets.nix;
+in
+{
+  flake.nixosConfigurations.Init = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+
+    specialArgs = {
+      inherit inputs;
+      userconf = userconf // sysconf // sshkeys // secrets;
+    };
+
+    modules = with self.nixosModules; [
+      PLACEHOLDERHardware
+      disko
+      preservation
+
+      base
+      user
+      git
+      networkmanager
+
+      {
+        boot.loader = {
+          systemd-boot.enable = true;
+          systemd-boot.configurationLimit = 10;
+          efi.canTouchEfiVariables = true;
+
+        };
+
+        system.stateVersion = userconf.state;
+
+        powerManagement.cpuFreqGovernor = "powersave";
+      }
+    ];
+  };
+}
