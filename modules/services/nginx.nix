24ef3baa0d8d8cfaa286e08da85ee9c04b592136
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-06-09 22:16:11 +0200

    removed nginx config for unused domains

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -26,28 +26,28 @@
             globalRedirect = userconf.topDom;
           };
 
-          ${userconf.aiDom} = {
-            forceSSL = true;
-            enableACME = true;
+          #${userconf.aiDom} = {
+          #  forceSSL = true;
+          #  enableACME = true;
 
-            locations."/" = {
+          #  locations."/" = {
 
-              proxyPass = "http://127.0.0.1:4180";
+          #    proxyPass = "http://127.0.0.1:4180";
 
-              proxyWebsockets = true; # needed if you need to use WebSocket
-              extraConfig =
-                # required when the target is also TLS server with multiple hosts
-                "proxy_ssl_server_name on;"
-                +
-                  # required when the server wants to use HTTP Authentication
-                  "proxy_pass_header Authorization;";
-            };
-          };
+          #    proxyWebsockets = true; # needed if you need to use WebSocket
+          #    extraConfig =
+          #      # required when the target is also TLS server with multiple hosts
+          #      "proxy_ssl_server_name on;"
+          #      +
+          #        # required when the server wants to use HTTP Authentication
+          #        "proxy_pass_header Authorization;";
+          #  };
+          #};
 
-          ${userconf.searchDom} = {
-            forceSSL = true;
-            enableACME = true;
-          };
+          #${userconf.searchDom} = {
+          #  forceSSL = true;
+          #  enableACME = true;
+          #};
         };
       };
     };
