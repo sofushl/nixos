a7231e072c89931e7674f91cc6e0bcedc7941d19
Sofus Lind <sofushl@proton.me>
2026-06-21 21:36:34 +0200

    Fixed ghname in sofushl.nix

--- lib/sofushl.nix
+++ lib/sofushl.nix
@@ -2,7 +2,7 @@
   username = "sofushl";
   displayname = "Sofus Lind";
   gitmail = "43791049+sofuslind@users.noreply.github.com";
-  ghname = "sofuslind";
+  ghname = "sofushl";
   email = "sofushl@proton.me";
   pinhash = "$y$j9T$/EEjqWS9HnfLJknQuxtzY.$jkG/dUYohj2VtSoacz4dbFhaEjqt61DEVfRDPD48so2";
 }
