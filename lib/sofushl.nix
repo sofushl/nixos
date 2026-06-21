1efad5189a39991de3047ecf9d79249637e972fd
Sofus Lind <sofushl@proton.me>
2026-06-21 21:41:13 +0200

    Update gitmail sofushl.nix

--- lib/sofushl.nix
+++ lib/sofushl.nix
@@ -1,7 +1,7 @@
 {
   username = "sofushl";
   displayname = "Sofus Lind";
-  gitmail = "43791049+sofuslind@users.noreply.github.com";
+  gitmail = "sofushl@proton.me";
   ghname = "sofushl";
   email = "sofushl@proton.me";
   pinhash = "$6$INVALID$PLACEHOLDER";
