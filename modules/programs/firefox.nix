f64527b4496c1eae4ca91e2ae82378608e9836fa
Sofus Lind <sofushl@proton.me>
2026-06-25 19:59:55 +0200

    fixes

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -92,12 +92,12 @@
           };
         };
       };
+    };
 
-      xdg.mimeApps = {
-        enable = true;
-        defaultApplications = {
-          "application/pdf" = "firefox.desktop";
-        };
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = "firefox.desktop";
       };
     };
 
