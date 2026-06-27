17e72e960eb3bfb0897cf7c90343f62c23cd8b5b
Sofus Lind <sofushl@proton.me>
2026-06-27 13:15:15 +0200

    made dns updater a systemd service

--- modules/hosts/WSL/soli.nix
+++ modules/hosts/WSL/soli.nix
@@ -25,6 +25,8 @@ in
       user
       neovim
       bash
+      firefox
+      wezterm
     ];
   };
 }
---
+++ modules/services/dns-updater.nix
@@ -0,0 +1,27 @@
+{
+  flake.nixosModules.dnsUpdater =
+    { userconf, pkgs, ... }:
+    let
+      pack = [ pkgs.wget ];
+    in
+    {
+      systemd.services.dns-update = {
+        path = pack;
+
+        script = map (
+          link: "wget -q --read-timeout=0.0 --waitretry=5 --tries=100 --background ${link}"
+        ) userconf.dnsUpdateLinks;
+
+        serviceConfig.Type = "oneshot";
+      };
+
+      systemd.timers.dns-update = {
+        wantedBy = [ "timers.target" ];
+
+        timerConfig = {
+          OnStartupSec = "1m";
+          OnUnitActiveSec = "30m";
+        };
+      };
+    };
+}
