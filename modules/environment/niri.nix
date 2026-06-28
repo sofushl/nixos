222f55ef999c6625d2857916b65975420826bd5d
Sofus Lind <sofushl@proton.me>
2026-06-28 22:25:39 +0200

    using niri-flake for setting up cross compatible niri setup

--- modules/environment/niri.nix
+++ modules/environment/niri.nix
@@ -1,4 +1,4 @@
-{ self, inputs, ... }:
+{ self, ... }:
 
 {
   flake.nixosModules.niri =
@@ -10,96 +10,109 @@
     }:
     {
       home-manager.users.${userconf.username}.imports = [ self.homeModules.niri ];
-      programs.dconf.enable = true;
+      #programs.niri.enable = true;
+      environment.systemPackages = [ pkgs.hyprlock ];
 
       # Fallback for xwayland-sattelite
       xdg.icons.fallbackCursorThemes = [ "Bibata-Modern-Classic" ];
     };
 
-  flake.homeModules.niri = { pkgs, ... }: {
+  flake.homeModules.niri =
+    {
+      pkgs,
+      inputs,
+      config,
+      ...
+    }:
+    {
+      imports = [ inputs.niri.homeModules.niri ];
 
-    xdg = {
-      enable = true;
-
-      configFile = {
-        "sunsetr/sunsetr.toml".source = ../../dotfiles/sunsetr.toml;
-        "niri/config.kdl".source = ../../dotfiles/niri.kdl;
-        "waybar".source = ../../dotfiles/waybar; # https://man.archlinux.org/man/waybar.5
-        "fuzzel/fuzzel.ini".source = ../../dotfiles/fuzzel.ini;
-        "hypr/hyprlock.conf".source = ../../dotfiles/hyprlock.conf;
+      programs.niri = {
+        enable = true;
+        package = pkgs.niri;
       };
 
-      portal = {
+      xdg = {
         enable = true;
-        extraPortals = [
-          pkgs.xdg-desktop-portal-gtk
-          pkgs.xdg-desktop-portal-wlr
-          pkgs.xdg-desktop-portal-gnome
+
+        configFile = {
+          "sunsetr/sunsetr.toml".source = ../../dotfiles/sunsetr.toml;
+          "niri/config.kdl".source = ../../dotfiles/niri.kdl;
+          "waybar".source = ../../dotfiles/waybar; # https://man.archlinux.org/man/waybar.5
+          "fuzzel/fuzzel.ini".source = ../../dotfiles/fuzzel.ini;
+          "hypr/hyprlock.conf".source = ../../dotfiles/hyprlock.conf;
+        };
+
+        portal = {
+          enable = true;
+          extraPortals = [
+            pkgs.xdg-desktop-portal-gtk
+            pkgs.xdg-desktop-portal-wlr
+            pkgs.xdg-desktop-portal-gnome
+          ];
+          config.common.default = [ "gtk" ];
+        };
+      };
+
+      gtk = {
+        enable = true;
+        colorScheme = "dark";
+
+        gtk3 = {
+          enable = true;
+          extraConfig.gtk-application-prefer-dark-theme = true;
+          colorScheme = "dark";
+        };
+
+        gtk4 = {
+          enable = true;
+          extraConfig.gtk-application-prefer-dark-theme = true;
+          colorScheme = "dark";
+        };
+      };
+
+      home = {
+        packages = with pkgs; [
+
+          # Environment applications
+
+          waybar
+          fuzzel
+          btop
+          wl-clipboard
+
+          # Environment controllers
+          pavucontrol
+          playerctl
+          brightnessctl
+          sunsetr
+
+          # Customization
+          bibata-cursors
+
+          # X11 support for niri
+          xwayland-satellite
         ];
-        config.common.default = [ "gtk" ];
+
+        sessionVariables = {
+          XCURSOR_THEME = "Bibata-Modern-Classic";
+          XCURSOR_SIZE = "24";
+          XDG_CURRENT_DESKTOP = "niri";
+        };
+        pointerCursor = {
+          name = "Bibata-Modern-Classic";
+          gtk.enable = true;
+          x11.enable = true;
+          size = 24;
+          package = pkgs.bibata-cursors;
+        };
       };
+
+      dconf.settings = {
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+        };
+      };
+
     };
-
-    gtk = {
-      enable = true;
-      colorScheme = "dark";
-
-      gtk3 = {
-        enable = true;
-        extraConfig.gtk-application-prefer-dark-theme = true;
-        colorScheme = "dark";
-      };
-
-      gtk4 = {
-        enable = true;
-        extraConfig.gtk-application-prefer-dark-theme = true;
-        colorScheme = "dark";
-      };
-    };
-
-    home = {
-      packages = with pkgs; [
-
-        # Environment applications
-        niri
-        waybar
-        fuzzel
-        hyprlock
-        btop
-        wl-clipboard
-
-        # Environment controllers
-        pavucontrol
-        playerctl
-        brightnessctl
-        sunsetr
-
-        # Customization
-        bibata-cursors
-
-        # X11 support for niri
-        xwayland-satellite
-      ];
-
-      sessionVariables = {
-        XCURSOR_THEME = "Bibata-Modern-Classic";
-        XCURSOR_SIZE = "24";
-        XDG_CURRENT_DESKTOP = "niri";
-      };
-      pointerCursor = {
-        name = "Bibata-Modern-Classic";
-        gtk.enable = true;
-        x11.enable = true;
-        size = 24;
-        package = pkgs.bibata-cursors;
-      };
-    };
-
-    dconf.settings = {
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-      };
-    };
-
-  };
 }
