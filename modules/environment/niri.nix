80dd2b8028c1a0717d29eff1bebc3cb16f6f6f09
Sofus Lind <sofushl@proton.me>
2026-06-28 12:59:34 +0200

    niri is now a home module

--- modules/environment/niri.nix
+++ modules/environment/niri.nix
@@ -15,37 +15,15 @@
 
       nixpkgs.overlays = [ inputs.niri.overlays.niri ];
 
+      home-manager.users.${userconf.username}.imports = [ self.homeModules.niri ];
+
       programs.niri = {
         enable = true;
         package = pkgs.niri;
       };
-
-      xdg = {
-        portal = {
-          wlr.enable = true;
-          enable = true;
-          extraPortals = [
-            pkgs.xdg-desktop-portal-gtk
-            pkgs.xdg-desktop-portal-wlr
-            pkgs.xdg-desktop-portal-gnome
-          ];
-
-          config.common.default = [ "*" ];
-        };
-
-        # Fallback for xwayland-sattelite
-        icons.fallbackCursorThemes = [ "Bibata-Modern-Classic" ];
-      };
+      # Fallback for xwayland-sattelite
+      xdg.icons.fallbackCursorThemes = [ "Bibata-Modern-Classic" ];
       environment = {
-
-        # Config imports
-        etc = {
-          "niri/config.kdl".source = ../../dotfiles/niri.kdl;
-          "xdg/waybar".source = ../../dotfiles/waybar; # https://man.archlinux.org/man/waybar.5
-          "xdg/fuzzel/fuzzel.ini".source = ../../dotfiles/fuzzel.ini;
-          "xdg/hypr/hyprlock.conf".source = ../../dotfiles/hyprlock.conf;
-        };
-
         systemPackages = with pkgs; [
 
           # Environment applications
@@ -74,50 +52,64 @@
           XDG_CURRENT_DESKTOP = "niri";
         };
       };
+    };
 
-      home-manager.users.${userconf.username} = {
-        programs.niri.settings = null;
+  flake.homeModules.niri = { pkgs, ... }: {
 
-        gtk = {
-          enable = true;
-          colorScheme = "dark";
+    xdg = {
+      enable = true;
 
-          gtk3 = {
-            enable = true;
-            extraConfig.gtk-application-prefer-dark-theme = true;
-            colorScheme = "dark";
-          };
+      configFile = {
+        "sunsetr/sunsetr.toml".source = ../../dotfiles/sunsetr.toml;
+        "niri/config.kdl".source = ../../dotfiles/niri.kdl;
+        "waybar".source = ../../dotfiles/waybar; # https://man.archlinux.org/man/waybar.5
+        "fuzzel/fuzzel.ini".source = ../../dotfiles/fuzzel.ini;
+        "hypr/hyprlock.conf".source = ../../dotfiles/hyprlock.conf;
+      };
 
-          gtk4 = {
-            enable = true;
-            extraConfig.gtk-application-prefer-dark-theme = true;
-            colorScheme = "dark";
-          };
-        };
-
-        dconf.settings = {
-          "org/gnome/desktop/interface" = {
-            color-scheme = "prefer-dark";
-          };
-        };
-
-        xdg = {
-          enable = true;
-
-          configFile."sunsetr/sunsetr.toml" = {
-            source = ../../dotfiles/sunsetr.toml;
-            force = true;
-          };
-
-          portal = {
-            enable = true;
-            extraPortals = [
-              pkgs.xdg-desktop-portal-gtk
-              pkgs.xdg-desktop-portal-wlr
-            ];
-            config.common.default = [ "gtk" ];
-          };
-        };
+      portal = {
+        enable = true;
+        extraPortals = [
+          pkgs.xdg-desktop-portal-gtk
+          pkgs.xdg-desktop-portal-wlr
+          pkgs.xdg-desktop-portal-gnome
+        ];
+        config.common.default = [ "gtk" ];
       };
     };
+
+    programs.niri.settings = null;
+
+    gtk = {
+      enable = true;
+      colorScheme = "dark";
+
+      gtk3 = {
+        enable = true;
+        extraConfig.gtk-application-prefer-dark-theme = true;
+        colorScheme = "dark";
+      };
+
+      gtk4 = {
+        enable = true;
+        extraConfig.gtk-application-prefer-dark-theme = true;
+        colorScheme = "dark";
+      };
+    };
+
+    home.pointerCursor = {
+      name = "Bibata-Modern-Classic";
+      gtk.enable = true;
+      x11.enable = true;
+      size = 24;
+      package = pkgs.bibata-cursors;
+    };
+
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+
+  };
 }
