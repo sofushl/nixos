333d42e78b3d83796dd32f743940e7e1187090d1
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-06-30 19:47:29 +0200

    cron job re-added, duplicate for now

--- modules/services/dns-updater.nix
+++ modules/services/dns-updater.nix
@@ -5,6 +5,14 @@
       pack = [ pkgs.wget ];
     in
     {
+
+      services.cron = {
+        enable = true;
+        systemCronJobs = map (
+          link: "0 * * * * wget -q --read-timeout=0.0 --waitretry=5 --tries=100 --background ${link}"
+        ) userconf.dnsUpdateLinks;
+      };
+
       systemd.services.dns-update = {
         path = pack;
 
