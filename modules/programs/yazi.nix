b7c1b35d4f850f2539f81a56294136e0e29a76ae
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-07-03 18:27:30 +0200

    added directory skipping to yazi

--- modules/programs/yazi.nix
+++ modules/programs/yazi.nix
@@ -1,5 +1,5 @@
 {
-  flake.homeModules.yazi = {
+  flake.homeModules.yazi = { pkgs, ... }: {
     programs = {
       yazi = {
         enable = true;
@@ -12,6 +12,50 @@
             sort_dir_first = true;
             sort_reverse = false;
           };
+        };
+
+        plugins = {
+          bypass = pkgs.fetchFromGitHub {
+            owner = "Rolv-Apneseth";
+            repo = "bypass.yazi";
+            rev = "main";
+            hash = "sha256-kho114UcjV90BDghZxUn2gZXtjY0tsE+C9ttlQZli6U=";
+          };
+        };
+
+        keymap = {
+          mgr.prepend_keymap = [
+            {
+              on = [ "l" ];
+              run = "plugin bypass smart-enter";
+              desc = "Open file / enter directory, skipping single-child dirs";
+            }
+            {
+              on = [ "<Right>" ];
+              run = "plugin bypass smart-enter";
+              desc = "Open file / enter directory, skipping single-child dirs";
+            }
+            {
+              on = [ "h" ];
+              run = "plugin bypass reverse";
+              desc = "Leave directory, skipping single-child dirs";
+            }
+            {
+              on = [ "<Left>" ];
+              run = "plugin bypass reverse";
+              desc = "Leave directory, skipping single-child dirs";
+            }
+            {
+              on = [ "L" ];
+              run = "enter";
+              desc = "Enter one directory level, no skipping";
+            }
+            {
+              on = [ "H" ];
+              run = "leave";
+              desc = "Leave one directory level, no skipping";
+            }
+          ];
         };
       };
     };
