aeb59de2e02e2b42126dae78bab825bf8132793b
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-07-06 21:33:38 +0200

    moved nixos folder into actual WSL dir

--- lib/WSLZbook.nix
+++ lib/WSLZbook.nix
@@ -1,7 +1,7 @@
 {
   sshkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcW+Me6dHxyYPWni5w9YBfHOunyGbTwrHHNK6MLtp2B sofushl@nixos";
   host = "WSLZbook";
-  path = "home/sofushl/Documents/nixos";
+  path = "home/sofushl/nixos";
   wifiboard = "eth0";
   state = "26.11";
 }
