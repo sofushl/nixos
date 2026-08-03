d7494ad625c4cea6b84d7615baae98522316103a
Sofus Lind <sofushl@proton.me>
2026-08-03 18:43:33 +0200

    Updated git service to allow wider range of app types including adding my rust email backend

--- lib/server.nix
+++ lib/server.nix
@@ -1,9 +1,13 @@
+{ pkgs, ... }:
+
 rec {
   topDom = "sofus.privatedns.org";
-  cloudDom = "cloud.sofus.privatedns.org";
+  cloudDom = "cloud.${topDom}";
   secondaryDom = "sofus.undo.it";
-  aiDom = "ai.sofus.privatedns.org";
-  mcDom = "mc.sofus.privatedns.org";
+  aiDom = "ai.${topDom}";
+  mcDom = "mc.${topDom}";
+  apiDom = "api.${topDom}";
+  emailApiDom = "email.${apiDom}";
 
   domains = [
 
@@ -12,8 +16,52 @@ rec {
     secondaryDom
     aiDom
     mcDom
+    apiDom
+    emailApiDom
 
   ];
 
   wifiboard = "eth";
+
+  gitServices = [
+    {
+      name = "portfolio";
+      subdir = "/dist";
+      repo = "https://github.com/sofushl/portfolio.git";
+      domain = topDom;
+      build = ''
+        npm i
+        npm run build
+      '';
+    }
+    {
+      name = "AbaCordium";
+      repo = "https://github.com/AbaCord/AbaCordium.git";
+      build = ''
+        npm i
+      '';
+      start = ''
+        npm start
+      '';
+    }
+    {
+      name = "email-backend";
+      repo = "https://github.com/sofushl/email-backend.git";
+      start = "./target/release/email-backend";
+      build = "cargo build --release --locked";
+      port = 3000;
+      domain = emailApiDom;
+      env = {
+        CARGO_HOME = "/var/www/email-backend/.cargo";
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+      };
+      pack = with pkgs; [
+        cargo
+        rustc
+        stdenv.cc
+        pkg-config
+        openssl
+      ];
+    }
+  ];
 }
--- modules/services/git-service.nix
+++ modules/services/git-service.nix
@@ -10,109 +10,90 @@
     # REQURIES PRESERVATION OF "/var/www"
 
     let
-      gitServices = [
-        {
-          name = "portfolio";
-          root = "/dist";
-          repo = "https://github.com/sofushl/portfolio.git";
-          domain = userconf.topDom;
-          build = ''
-            npm i
-            npm run build
-          '';
-          start = "";
-        }
-        {
-          name = "AbaCordium";
-          repo = "https://github.com/AbaCord/AbaCordium.git";
-          root = "/";
-          domain = null;
-          build = ''
-            npm i
-          '';
-          start = ''
-            npm start
-          '';
-        }
-      ]
-      ++ userconf.secretServices;
+      serviceDefaults = {
+        name = throw "gitServices: name is required";
+        subdir = "/";
+        repo = throw "gitServices: repo is required";
+        domain = null;
+        port = null;
+        build = "";
+        start = null;
+        env = { };
+        pack = [ pkgs.nodejs ];
+      };
+      withDefaults = service: serviceDefaults // service;
+      gitServices = map withDefaults (userconf.gitServices ++ userconf.secretServices);
 
-      gitSites = lib.filter (service: service.domain != null) gitServices;
+      forwarded = lib.filter (service: service.domain != null) gitServices;
+      running = lib.filter (service: service.start != null) gitServices;
 
       pack = with pkgs; [
         git
-        nodejs
         bash
       ];
     in
     {
       environment.systemPackages = pack;
 
-      systemd.services = {
-        git-service = {
-          path = pack;
-
-          script = ''
-            mkdir -p /var/www
-
-            ${lib.concatMapStringsSep "\n" (service: ''
-
+      systemd.services = lib.listToAttrs (
+        map (service: {
+          name = "git-update-${service.name}";
+          value = {
+            path = pack ++ service.pack;
+            environment = {
+              HOME = "/var/www/${service.name}";
+            }
+            // service.env;
+            script = ''
+              mkdir -p /var/www
               if [ ! -d /var/www/${service.name}/.git ]; then
                 git clone ${service.repo} /var/www/${service.name}
-                
+
                 cd /var/www/${service.name}
                 ${service.build}
-
                 systemctl restart app-${service.name}.service
-
               else
                 before=$(git -C /var/www/${service.name} rev-parse HEAD)
-
                 echo "before: $before"
-
                 git -C /var/www/${service.name} fetch origin
                 git -C /var/www/${service.name} reset --hard origin/HEAD
                 git -C /var/www/${service.name} checkout main
-                
+
                 cd /var/www/${service.name}
-                
+
                 after=$(git rev-parse HEAD)
-
                 echo "after:  $after"
-
                 if [ "$before" != "$after" ]; then
                   ${service.build}
-
                   systemctl restart app-${service.name}.service
                 fi
               fi
-            '') gitServices}
-          '';
-
-          serviceConfig.Type = "oneshot";
-        };
-      }
-      // lib.listToAttrs (
-        map (service: {
+            '';
+            serviceConfig.Type = "oneshot";
+          };
+        }) gitServices
+        ++ map (service: {
           name = "app-${service.name}";
           value = {
-            path = pack;
-
+            path = pack ++ service.pack;
+            environment =
+              lib.optionalAttrs (service.port != null) {
+                PORT = toString service.port;
+              }
+              // service.env;
             script = ''
               cd /var/www/${service.name}
               ${service.start}
             '';
-
             serviceConfig = {
               Type = "simple";
               Restart = "always";
               RestartSec = 5;
               User = "nginx";
             };
-
             wantedBy = [ "multi-user.target" ];
           };
-        }) gitServices
+        }) running
       );
 
       systemd.timers.git-service = {
@@ -130,14 +111,23 @@
         map (site: {
           name = site.domain;
           value = {
-            root = "/var/www/${site.name}${site.root}";
             forceSSL = true;
             enableACME = true;
-            locations."/" = {
-              tryFiles = "$uri $uri/ /index.html";
-            };
-          };
-        }) gitSites
+          }
+          // (
+            if (site.port or null) != null then
+              {
+                locations."/" = {
+                  proxyPass = "http://127.0.0.1:${toString site.port}";
+                };
+              }
+            else
+              {
+                root = "/var/www/${site.name}${site.subdir}";
+                locations."/".tryFiles = "$uri $uri/ /index.html";
+              }
+          );
+        }) forwarded
       );
     };
 }
