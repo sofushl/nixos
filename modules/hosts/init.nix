d936701d042f92352ac75266260837b30cecec41
Sofus Lind <sofushl@proton.me>
2026-08-10 04:43:49 +0200

    even smaller D:

--- modules/hosts/init.nix
+++ modules/hosts/init.nix
@@ -6,11 +6,22 @@
       self.nixosModules.disko
       {
         nixpkgs.hostPlatform = "x86_64-linux";
-        boot.loader.systemd-boot.enable = true;
-        hardware.enableRedistributableFirmware = true;
-        networking.networkmanager.enable = true;
         system.stateVersion = "26.11";
+
+        boot.loader.systemd-boot.enable = true;
         users.users.root.initialPassword = "p";
+
+        # networking
+        networking.wireless.iwd.enable = true;
+        networking.wireless.iwd.settings.General.EnableNetworkConfiguration = true;
+        hardware.enableRedistributableFirmware = true; # drop if installing over ethernet
+
+        # size trimming
+        i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
+        documentation.enable = false;
+        documentation.nixos.enable = false;
+        environment.defaultPackages = [ ];
+        programs.command-not-found.enable = false;
       }
     ];
   };
