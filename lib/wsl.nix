53d24e38b10e72308fe26c55944db0904f2f323b
Sofus Lind <sofushl@proton.me>
2026-08-13 22:02:57 +0200

    updated ssh key for wsl

--- lib/wsl.nix
+++ lib/wsl.nix
@@ -1,5 +1,5 @@
 {
-  sshkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcW+Me6dHxyYPWni5w9YBfHOunyGbTwrHHNK6MLtp2B sofushl@nixos";
+  sshkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4ZvT/w/qm2UO4zxxdhXZDVFjqORwVpJiUgpSl3CdjC sofushl@Zbook";
   host = "Zbook";
   path = "home/sofushl/nixos";
   wifiboard = "eth0";
