396882bed914f1200cda63876ba1c9fdf9a9efb4
Sofus Lind <sofushl@proton.me>
2026-09-11 16:34:24 +0200

    made c file into librarypath with gnumake

--- modules/develop/c.nix
+++ modules/develop/c.nix
@@ -1,12 +1,19 @@
 {
   flake.nixosModules.c =
     { userconf, pkgs, ... }:
-    {
-      environment.systemPackages = with pkgs; [
+    let
+      libs = with pkgs; [
         gcc
         clang
         clang-tools
         cmake
+        gnumake
       ];
+    in
+    {
+      environment.systemPackages = libs;
+
+      programs.nix-ld.enable = true;
+      programs.nix-ld.libraries = libs;
     };
 }
