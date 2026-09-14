05ab8d9d676de98b1d98c3d2bc149d7906efe777
Sofus Lind <sofushl@proton.me>
2026-09-14 23:24:59 +0200

    Updated lualine with wordcount

--- dotfiles/nvim/plugin/lualine.lua
+++ dotfiles/nvim/plugin/lualine.lua
@@ -1,6 +1,47 @@
+local function wordcount()
+	local wc = vim.fn.wordcount()
+	return (wc.visual_words or wc.words)
+end
+
 require("lualine").setup({
-	options = { theme = "auto", globalstatus = true, section_separators = "", component_separators = "" },
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		always_divide_middle = true,
+		always_show_tabline = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+			refresh_time = 16, -- ~60fps
+			events = {
+				"WinEnter",
+				"BufEnter",
+				"BufWritePost",
+				"SessionLoadPost",
+				"FileChangedShellPost",
+				"VimResized",
+				"Filetype",
+				"CursorMoved",
+				"CursorMovedI",
+				"ModeChanged",
+			},
+		},
+	},
 	sections = {
-		lualine_c = { "filename", "lsp_progress" },
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { "filename" },
+		lualine_x = {
+			wordcount,
+			"encoding",
+			"fileformat",
+			"filetype",
+		},
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
 })
