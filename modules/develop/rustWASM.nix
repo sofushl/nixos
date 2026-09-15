0a5f6b30a3a61d1d28d1a2f21ac7e7c815b1efab
Sofus Lind <sofushl@proton.me>
2026-09-16 00:55:16 +0200

    Updated rust module

---
+++ modules/develop/rustWASM.nix
@@ -0,0 +1,44 @@
+{
+  flake.nixosModules.rustWASM =
+    { lib, pkgs, ... }:
+    let
+      libs = with pkgs; [
+        libxkbcommon
+        vulkan-loader
+        libGL
+        wayland
+        libx11
+        libxcursor
+        libxi
+      ];
+      rpath = lib.makeLibraryPath libs;
+    in
+    {
+      environment = {
+        systemPackages =
+          with pkgs;
+          [
+            rustc
+            cargo
+            clippy
+            rust-analyzer
+            rustlings
+
+            cargo-generate
+            pkg-config
+            openssl
+
+            lld
+            trunk
+            cargo-wasi
+            tailwindcss_4
+          ]
+          ++ libs;
+        variables = {
+          RUSTFLAGS = "-C link-arg=-Wl,-rpath,${rpath}";
+        };
+      };
+      programs.nix-ld.enable = true;
+      programs.nix-ld.libraries = libs;
+    };
+}
